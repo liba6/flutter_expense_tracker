df3a3a8d9a13c6afe812b588a000acd40338a48a
liba6 <lginsburg@gmail.com>
2023-08-25 12:53:13 +0200

    feat(expense) : Add bar to show expenses

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,30 @@ import 'package:flutter_expense_tracker/models/expense.dart';
 import 'package:flutter_expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.registeredExpenses});
+  const ExpensesList({
+    super.key,
+    required this.registeredExpenses,
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> registeredExpenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: registeredExpenses.length,
-      itemBuilder: (context, index) => ExpenseItem(registeredExpenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(registeredExpenses[index]),
+        background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+            margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal)),
+        onDismissed: (direction) {
+          onRemoveExpense(registeredExpenses[index]);
+        },
+        child: ExpenseItem(registeredExpenses[index]),
+      ),
     );
   }
 }
---
+++ lib/widgets/providers/origin_provider.dart
@@ -0,0 +1,10 @@
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// final cityProvider = Provider<String>((ref) => "NYC");
+
+// final stateProvider = Provider<String>((ref) => "NYS");
+
+// final countryProvider = Provider<String>((ref) => 'USA');
+
+//  // final sportProvider =
+//   //     Provider<List<String>>((ref) => ['soccer', 'basketball', 'tennis']);
